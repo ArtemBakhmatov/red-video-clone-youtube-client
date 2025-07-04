90eeb59c811e438774386d586097885c4f5db14d
artem_bakhmatov <ip.bakhmatov@mail.ru>
2025-07-04 20:02:19 +0300

    Layout (новый роутинг Next js)

---
+++ src/components/layout/content/Content.tsx
@@ -0,0 +1,13 @@
+import type { PropsWithChildren } from "react";
+import { Header } from "./header/Header";
+
+export function Content( { children }: PropsWithChildren<unknown>) {
+  return (
+    <div>
+      <Header />
+      <section>
+        { children }
+      </section>
+    </div>
+  )
+}
